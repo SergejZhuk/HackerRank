c8c111a77049f5bae78d6029aaabfa0d0150b67a
Sergej.Zhuk <sergej.zhuk.89@mail.ru>
2023-02-07 19:08:42 +0300

    solved

---
+++ src/JavaStringTokens/Solution.java
@@ -0,0 +1,20 @@
+package JavaStringTokens;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        String[] words = s.trim().split("[ !,?._'@]");
+        int sum = 0;
+        for (int i = 0; i < words.length; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+        // Write your code here.
+        // scan.close();
+
+    }
+}
