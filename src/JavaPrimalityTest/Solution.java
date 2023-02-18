2902509bbce5205bda77202e0fc4d95034442489
Sergej.Zhuk <sergej.zhuk.89@mail.ru>
2023-02-18 16:42:21 +0300

    solved

---
+++ src/JavaPrimalityTest/Solution.java
@@ -0,0 +1,20 @@
+package JavaPrimalityTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String n = bufferedReader.readLine();
+        BigInteger value = new BigInteger(n);
+        if (value.isProbablePrime(1000)) {
+            System.out.println("prime");
+        } else {
+            System.out.println("not prime");
+        }
+        bufferedReader.close();
+    }
+}
--- src/Test/Test.java
+++ src/Test/Test.java
@@ -1,15 +1,14 @@
 package Test;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        while(in.hasNext()){
-            int str = in.nextInt();
-            System.out.println(str);
-            //String IP = in.next();
-          //  System.out.println(IP.matches(new MyRegex().pattern));
+    public static void main(String[] args) throws IOException {
+
+        while (true) {
+            //int x = System.in.read();
+            System.out.println(System.in.read());
         }
     }
 }
